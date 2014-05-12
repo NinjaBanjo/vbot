20020a4bd90ba14b6baccdc3f8ebbb032185a260
Emerson Veenstra <emerson@emersonveenstra.net>
2014-05-12 13:45:44 -0400

    Forget to change the listener

--- vbot.js
+++ vbot.js
@@ -20,7 +20,7 @@ var JSBot = function(profile) {
 
 	Bot.call(this, profile);
 	this.set_log_level(this.LOG_ALL);
-	this.set_trigger("emersonbot: "); // Exclamation
+	this.set_trigger("vbot: "); // Exclamation
 };
 
 
