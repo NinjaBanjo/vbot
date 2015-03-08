8705fc6e0c80ba9c3dda005916a4e89ca8bf06fa
Emerson Veenstra <emerson@emersonveenstra.net>
2015-03-08 16:36:42 -0400

    Fixed error check for commands

--- vbot.js
+++ vbot.js
@@ -126,7 +126,8 @@ Bot.prototype.receive_data = function(chunk) {
         message = this.parse_raw(message);
 
         if (message !== false) {
-            switch (message.command.toUpperCase()) {
+            if (typeof message.command === "string") message.command = message.command.toUpperCase();
+            switch (message.command) {
                 case 1: // RPL_WELCOME
                     for (var i=0; i<this.profile.channels.length; i++) {
                         this.send_raw("JOIN " + this.profile.channels[i]);
